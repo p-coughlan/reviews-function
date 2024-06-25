0794dad35b669544f025dc2a15d2d509b228721a
p-coughlan <patrickcoughlan@yahoo.co.uk>
2024-06-25 19:37:22 +0100

    add event listeners on buttons and refactor code for 'show person'

--- app.js
+++ app.js
@@ -48,9 +48,42 @@ let currentItem = 0;
 
 //load initial item
 window.addEventListener('DOMContentLoaded', function(){
-  const item = reviews[currentItem];
+  showPerson(currentItem);
+});
+
+//show person based on item
+
+function showPerson(person){
+  const item = reviews[person];
   img.src = item.img;
   author.textContent = item.name;
   job.textContent = item.job;
   info.textContent = item.text;
+}
+
+//show next person
+
+nextBtn.addEventListener('click', function(){
+  currentItem++;
+  if(currentItem > reviews.length - 1){
+    currentItem = 0;
+  }
+  showPerson(currentItem);
+});
+
+//show previous person
+prevBtn.addEventListener('click', function(){
+  currentItem--;
+  if(currentItem < 0){
+    currentItem = reviews.length - 1;
+  }
+  showPerson(currentItem);
+});
+
+
+//show random person
+//uses the Math.random() method to generate a random number between 0 and 1. We then multiply that number by the length of the reviews array to get a random number between 0 and the last index of the array. We then use Math.floor() to round down to the nearest whole number.
+randomBtn.addEventListener('click', function(){
+  currentItem = Math.floor(Math.random() * reviews.length);
+  showPerson(currentItem);
 });
